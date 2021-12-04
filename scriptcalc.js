0607a779b4705307e4329b778e3cb6d2483fdfc1
Evgeni Egorov <avuron@gmail.com>
2021-12-04 12:50:31 +0200

    Task Update
    Calcs Done

---
+++ scriptcalc.js
@@ -0,0 +1,59 @@
+//read number1, operator and number2 from HTML page
+function getValuesFromCalculator123(){
+    let number1 = parseInt (document.getElementById("number1").value);
+    let number2 = parseInt (document.getElementById("number2").value);
+    let numbers = number1 + number2;
+    let operator = (document.getElementById("operator").value);
+    console.log(numbers);
+   // console.log(operator);
+
+    //alert("read values: " + number1 + operator + number2);
+    let returnvalue = calculate(number1,operator,number2);
+
+    document.getElementById("result").innerHTML=returnvalue;
+}
+
+//This function for calculator with radio buttons
+function getValuesFromCalculator2() {
+    let number1 = parseInt (document.getElementById("number1").value);
+    let number2 = parseInt (document.getElementById("number2").value);
+    var ans = "xxx";
+    
+
+    if (document.getElementById("+").checked) {
+        ans = calculate(number1, '+', number2);
+    }
+    if (document.getElementById("-").checked) {
+        ans = calculate(number1, '-', number2);
+    }
+    if (document.getElementById("*").checked) {
+        ans = calculate(number1, '*', number2);
+    }
+    if (document.getElementById("/").checked) {
+        ans = calculate(number1, '/', number2);       
+    }
+    document.getElementById("result").value=ans;
+    
+    
+
+}
+
+function calculate(numberX, operator, numberY){
+    let returnvalue = 0;
+    switch (operator){
+        case "+":
+            returnvalue= numberX+numberY;
+            break;
+        case "-":
+            returnvalue=numberX-numberY;
+            break;
+        case "*":
+            returnvalue=numberX*numberY;
+            break;
+        case "/":
+            returnvalue=numberX/numberY;
+            break;
+    }
+    return returnvalue;
+
+}
